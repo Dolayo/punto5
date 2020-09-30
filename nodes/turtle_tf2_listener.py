4e18a946b0b82c66c3acffc5263ccf1422c49c76
Dolayo <d.olayo.andres@gmail.com>
2020-09-30 16:52:34 +0200

    version a implementar

--- nodes/turtle_tf2_listener.py
+++ nodes/turtle_tf2_listener.py
@@ -6,6 +6,15 @@ import tf2_ros
 import geometry_msgs.msg
 import turtlesim.srv
 
+def my_callback(event):
+    global flag3 
+    flag3 = False
+    print(flag3, 'HE ENTRADOOOOOOOOOOO')
+    spawner(2, 4, 0, turtle_name2)
+    return
+
+
+
 if __name__ == '__main__':
     rospy.init_node('tf2_turtle_listener')
 
@@ -17,7 +26,8 @@ if __name__ == '__main__':
     turtle_name = rospy.get_param('turtle', 'turtle2')
     turtle_name2 = rospy.get_param('turtle', 'turtle3')
     spawner(4, 2, 0, turtle_name)
-    spawner(2, 4, 0, turtle_name2)
+    
+    
 
     turtle_vel = rospy.Publisher('%s/cmd_vel' % turtle_name, geometry_msgs.msg.Twist, queue_size=1)
     turtle_vel2 = rospy.Publisher('%s/cmd_vel' % turtle_name2, geometry_msgs.msg.Twist, queue_size=1)
@@ -25,28 +35,25 @@ if __name__ == '__main__':
 
     flag1 = False
     flag2 = False
+    flag3 = True
+    #antes = rospy.Time.now()
+    if(flag3):
+        rospy.Timer(rospy.Duration(4), my_callback,oneshot=True ,reset=True)
+        print(flag3)
 
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
         try:
-           
+
+            #if((rospy.Time.now() - antes > 2.0) and flag3):
+                #spawner(2, 4, 0, turtle_name2)
+                #antes = rospy.Time.now()
+                #flag3 = False
+            
+            print('estoy rn rl LOOOOOOP', flag3)
             past = rospy.Time.now() - rospy.Duration(1.0)#aqui por el tiempo que vaya por detras
-            trans = tfBuffer.lookup_transform_full(
-                target_frame=turtle_name,
-                target_time=rospy.Time.now(),
-                source_frame='carrot1',
-                source_time=past,
-                fixed_frame='world',
-                timeout=rospy.Duration(1.0)
-                )
-            trans2 = tfBuffer.lookup_transform_full(
-                target_frame=turtle_name2,
-                target_time=rospy.Time.now(),
-                source_frame='carrot2',
-                source_time=past,
-                fixed_frame='world',
-                timeout=rospy.Duration(1.0)
-                )
+            trans = tfBuffer.lookup_transform(turtle_name, 'carrot1', rospy.Time())
+            trans2 = tfBuffer.lookup_transform(turtle_name2, 'carrot2', rospy.Time())
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
             rate.sleep()
             continue
@@ -72,8 +79,8 @@ if __name__ == '__main__':
             flag2 = True
 
         if (flag2):
-            msg2.angular.z = 4 * math.atan2(trans2.transform.translation.y, trans2.transform.translation.x)
-            msg2.linear.x = 0.5 * math.sqrt(trans2.transform.translation.x ** 2 + trans2.transform.translation.y ** 2)
+            msg2.angular.z = 6 * math.atan2(trans2.transform.translation.y, trans2.transform.translation.x)
+            msg2.linear.x = 3 * math.sqrt(trans2.transform.translation.x ** 2 + trans2.transform.translation.y ** 2)
 
         
         turtle_vel.publish(msg)
